038cba1a21bf60c8aa30bd53fbad93a4c41a9b25
santoshi2112 <60030154+santoshi2112@users.noreply.github.com>
2020-07-23 16:11:18 +0530

    final commit

--- StartA/src/main/java/com/epam/StartA/Task10_3.java
+++ StartA/src/main/java/com/epam/StartA/Task10_3.java
@@ -1,26 +1,21 @@
 package com.epam.StartA;
 import java.util.stream.Stream;
 import java.util.*;
-import java.util.function.Predicate;
+
+
+interface Checker{
+	void check(Stream<String> s);
+}
 
 public class Task10_3
 {
 	
-	public static List<String> checkStr(List<String> al, Predicate<String> p){
-		List<String> res = new ArrayList<String>();
-		for(String i:al) {
-			if(p.test(i)) {
-				res.add(i);
-			}
-		}
-		return res;
-	}
     public static void main( String[] args )
     {	
     	//checking whether the scanned strings satisfy the condition-
     	//starting with "a" and
     	//have length of exact 3
-    	//using stream operations and functional interface
+    	//using stream operations and lambdas
     	
     	Scanner sc=new Scanner(System.in);
     	System.out.println("Enter five strings");
@@ -30,11 +25,9 @@ public class Task10_3
     	}
     	sc.close();
     	Stream<String> stream2 = al.stream();
-    	System.out.println("The result using predicate functional interface - ");
-    	List<String> res=checkStr(al,(String i)->(i.startsWith("a") && i.length()==3));
-    	System.out.println(res);
-    	System.out.println("The result using  stream operations -");
-    	stream2.filter(s->s.startsWith("a")).filter(s->s.length()==3).forEach(s->System.out.println(s));
-    	
+ 
+    	System.out.println("The result is -");
+    	Checker ch=i->stream2.filter(s->s.startsWith("a")).filter(s->s.length()==3).forEach(s->System.out.println(s));
+    	ch.check(stream2);
     }
 }
